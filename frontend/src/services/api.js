5e5f942549ff958d6677e25ee5fabe83b54786b4
Maximiano11 <129424957+Maximiano11@users.noreply.github.com>
2025-10-11 00:19:09 -0300

    Add files via upload

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.MODE === "production"
-    ? "https://burguer-night.onrender.com" // backend no Render
-    : "http://localhost:4001";             // backend local
+const isRender = window.location.hostname.includes("onrender.com");
+
+const baseURL = isRender
+  ? "https://burguer-night.onrender.com" // backend no Render
+  : "http://localhost:4001";              // backend local
 
 export const api = axios.create({
   baseURL,
