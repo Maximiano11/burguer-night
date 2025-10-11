f2779c4edc1420e537334b3ccaa1d6647e88a60b
Maximiano11 <129424957+Maximiano11@users.noreply.github.com>
2025-10-11 00:52:12 -0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../build', // importante pro Render achar o build final
-  }
-})
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
+});
