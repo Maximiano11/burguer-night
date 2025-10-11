ee4f42f02e4f5ab6167c7d4822e0e781858a2d2b
Maximiano11 <129424957+Maximiano11@users.noreply.github.com>
2025-10-10 21:21:03 -0300

    Add files via upload

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NODE_ENV === "production"
+  import.meta.env.MODE === "production"
     ? "https://burguer-night.onrender.com" // backend no Render
-    : "http://localhost:4001";            // backend local
+    : "http://localhost:4001";             // backend local
 
 export const api = axios.create({
   baseURL,
-  // withCredentials: true, // só se realmente precisar de cookies
+  withCredentials: true,
 });
